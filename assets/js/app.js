14d42e887253101e61685acb2f88b339e3b3c0f9
Mahdi Alikhani2005 <mahdialikhaniapr2005@gmail.com>
2024-07-01 11:28:10 +0330

    Make_data

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+import { data, menData, womenData, kidsData } from "../..//utils/data.js";
+
+const headerData = data.slice(0, 5);
+console.log(headerData);
+console.log(menData);
+console.log(womenData);
+console.log(kidsData);
