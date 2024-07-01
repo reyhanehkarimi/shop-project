449f35e4bf9b1ec70547865ea5c6ff64602df820
Mahdi Alikhani2005 <mahdialikhaniapr2005@gmail.com>
2024-07-01 11:35:34 +0330

    Make_Utility-folders

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,9 @@
 import { data, menData, womenData, kidsData } from "../..//utils/data.js";
-
+import { createElementRepo } from "../..//utils/domUtils.js";
+import {
+  saveToLocalStorage,
+  getFromLocalStorage,
+} from "../../utils/storageUtils.js";
 const headerData = data.slice(0, 5);
 console.log(headerData);
 console.log(menData);
---
+++ utils/storageUtils.js
@@ -0,0 +1,9 @@
+const saveToLocalStorage = (key, value) => {
+  localStorage.setItem(key, JSON.stringify(value));
+};
+
+const getFromLocalStorage = (key) => {
+  return JSON.parse(localStorage.getItem(key));
+};
+
+export { saveToLocalStorage, getFromLocalStorage };
